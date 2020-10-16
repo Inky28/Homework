a26bc7d07d90b2e5d97789e3e0605b38d6d35e36
Inky Igop <Igop2828@gmail.com>
2020-10-16 11:35:59 +0300

    task06

---
+++ Homework_05/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Розробити власний тип даних для розвязування  лінійного рівняння А*Х + В = 0. 
+ У класі визначити 
+ Конструктор з 2-ма параметрами, що приймає коефіціенти рівняння
+ Метод пошуку розвязку рівняння
+ Статичний метод Parse( ), який приймає рядок з значеннями коефіціентів, розділених  пропуском(чи табуляцією)
+ У випадку передачі в метод рядка недопустимого формату генерується виключення FormatException.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearEquation le = new LinearEquation(0, 6);
+            le.Parse("2x + 3");
+
+            try
+            {
+                le.Parse("2");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            le.Print();
+        }
+    }
+}
