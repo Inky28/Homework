d33855c45692d6369f89b5023bcd43a300dadb41
Inky Igop <Igop2828@gmail.com>
2020-10-23 14:09:23 +0300

    task12

---
+++ Homework_11/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ 1. Дано текстовий файл з cs - програмою. (будь-яка домашня робота, або взяти із репозиторію)
+Вилучити однорядкові  коментарі(утворити інший  cs - файл)        var  val =12; //this is comment
+Знайти службові слова С#, які зустрілися у файлі та їх кількість
+	
+2. Утворити рваний масив([][]) з байтів. Записати його у файл та прочитати з файлу. Написати відповідні методи. 
+Використати клас FileStream.
+ */
+
+namespace Homework_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> doc = new List<string>();
+            try
+            {
+                using (StreamReader reading = File.OpenText("comp.cs"))
+                {
+                    string str;
+                    while ((str = reading.ReadLine()) != null)
+                    {
+                        if (str.Contains("//"))
+                        {
+                            int index = str.LastIndexOf("//");
+                            doc.Add(str.Substring(0, index));
+                        }
+                        else
+                        {
+                            doc.Add(str);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR_1");
+            }
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter("no_commit.cs"))
+                {
+                    foreach (var item in doc)
+                    {
+                        writer.WriteLine(item);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("ERROR_2");
+            }
+        }
+    }
+}
---
+++ Homework_11/bin/Debug/comp.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_07
+{
+    class Comp
+    {
+        private int countDisk;
+        private int countPrintDevice;
+        private List<Disk> disks;
+        private List<IPrintInformation> printDevice;
+		// test
+        public void AddDevice(IPrintInformation si)
+        {
+            printDevice.Add(si);
+            countPrintDevice = printDevice.Count();
+        }
+		// commit_11
+        public void AddDisc(Disk d)
+        {
+            disks.Add(d);
+            countDisk = disks.Count();
+        }
+		// commit_22
+        public bool CheckDisk(string device)
+        {
+            foreach (var item in disks)
+            {
+                if (item.GetName().Equals(device))  // commit -112212333
+                    return true;
+            }
+            return false;
+        }
+		// commit_33
+        public Comp()
+        {
+            disks = new List<Disk>();
+            printDevice = new List<IPrintInformation>();
+        }
+		// commit_a234
+        public void InsertReject(string device, bool b) // commit -11aa22
+        {
+            foreach (var item in disks)
+            {
+                if (item.GetName().Equals(device) && item is IRemoveableDisk)
+                {
+                    if (b)
+                        (item as IRemoveableDisk).Insert();
+                    else
+                        (item as IRemoveableDisk).Reject();
+                }
+            }
+        }
+		// commit_3123123
+        public bool PrintInfo(string text, string device)
+        {
+            foreach (var item in printDevice)
+            {
+                if (item.GetName().Equals(device)) // commit -1122
+                {
+                    item.Print(text);
+                    return true;
+                }
+            }
+            return false;
+        }
+		// commit_q1w2e3r4
+        public string ReadInfo(string device)
+        {
+            foreach (var item in disks)
+            {
+                if (item.GetName().Equals(device))
+                    return item.Read();
+            }
+            return "";
+        }
+
+        public void ShowDisk()
+        {
+            foreach (var item in disks)
+            {
+                Console.WriteLine(item.GetName());
+            }
+        }
+
+        public void ShowPrintDevice()
+        {
+            foreach (var item in printDevice)
+            {
+                Console.WriteLine(item.GetName());
+            }
+        }
+
+        public bool WriteInfo(string text, string showDevice)
+        {
+            foreach (var item in printDevice)
+            {
+                if (item.GetName().Equals(showDevice))
+                {
+                    item.Print(text);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
